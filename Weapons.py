6dfa8a524bb92a9a7ae1080a8b472327662b518e
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2016-04-02 21:15:48 -0700

    weapons
    so far...

--- Weapons.py
+++ Weapons.py
@@ -1,19 +1,39 @@
-class item(object):
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description 
-        
-class weapons(item):
-    def __init__(self, name, description, damage, weight):
-        super(weapons, self).__init__(name, description)
+#WEAPONS AT USE 
+class Item(object):
+    def __init__(self, name):
+        self.name = name 
+
+#SUPERCLASS                
+class Weapons(Item):
+    
+    def __init__(self, name, damage, weight):
+        super(Weapons, self).__init__(name)
         self.damage = damage
         self.weight = weight
-
-class two_hand(weapons):
-         def __init__(self, name = 'bow', description = 'kill' , damage = 50, weight = 100):
-            super(two_hand, self).__init__(name = 'bow', description = 'kill' , damage = 50, weight = 100)
-            
-         def bow(self):
-             print '%s can kill' 
-            
+         
+#SUB_CLASSES        
+class two_hand(Weapons):
+    def __init__(self, name, damage = 100, weight = 150):
+        super(two_hand, self).__init__(name, damage = 100, weight = 150)
         
+class two_hand2(Weapons):
+    def __init__(self, name, damage = 50, weight = 80):
+        super(two_hand2, self).__init__(name, damage = 50, weight = 80)
+        
+class two_hand3(Weapons):
+    def __init__(self, name, damage = 55, weight = 100):
+        super(two_hand3, self).__init__(name, damage = 55, weight = 100)
+        
+        
+class one_hand(Weapons):
+    def __init__(self, name, damage = 10, weight = 15):
+        super(one_hand, self).__init__(name, damage = 10, weight = 15)
+        
+    
+        
+sword = two_hand(Weapons) 
+axe = two_hand2(Weapons)
+cross_bow = two_hand3(Weapons)
+dagger = one_hand(Weapons) 
+
+#CONSUMABLES        
